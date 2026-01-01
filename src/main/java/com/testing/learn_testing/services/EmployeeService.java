ecb0eba064557ec3d29b2998ed59c81159a28a09
Akshit <akshit07032001@gmail.com>
2026-01-01 12:33:17 +0530

    add: basic mvc

---
+++ src/main/java/com/testing/learn_testing/services/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.testing.learn_testing.services;
+
+
+import com.testing.learn_testing.dto.EmployeeDto;
+
+public interface EmployeeService {
+
+    EmployeeDto getEmployeeById(Long id);
+    EmployeeDto createNewEmployee(EmployeeDto employeeDto);
+    EmployeeDto updateEmployee(Long id, EmployeeDto employeeDto);
+    void deleteEmployee(Long id);
+}
