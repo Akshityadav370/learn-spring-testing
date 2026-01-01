1d43c50191aaab37a56be179ba68f1d505ae33fb
Akshit <akshit07032001@gmail.com>
2026-01-01 12:19:12 +0530

    init

---
+++ src/main/java/com/testing/learn_testing/LearnTestingApplication.java
@@ -0,0 +1,13 @@
+package com.testing.learn_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testing/learn_testing/LearnTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.learn_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
