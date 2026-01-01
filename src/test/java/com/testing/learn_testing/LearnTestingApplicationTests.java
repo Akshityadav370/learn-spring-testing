74af62c7a76f01b42b4e0cab09695bc6b83ac553
Akshit <akshit07032001@gmail.com>
2026-01-01 12:26:00 +0530

    junit and assertj into

--- src/test/java/com/testing/learn_testing/LearnTestingApplicationTests.java
+++ src/test/java/com/testing/learn_testing/LearnTestingApplicationTests.java
@@ -1,13 +1,75 @@
 package com.testing.learn_testing;
 
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@Slf4j
 class LearnTestingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeEach
+    void setUp() {
+        log.info("Starting the method, setting up config");
+    }
+
+    @AfterEach
+    void tearDown() {
+        log.info("Tearing down the method");
+    }
+
+    @BeforeAll
+    static void setUpOnce() {
+        log.info("Setup Once...");
+    }
+
+    @AfterAll
+    static void tearDownOnce() {
+        log.info("Tearning down all...");
+    }
+
+    @Test
+    void testNumberOne() {
+        int a = 5;
+        int b = 3;
+
+        int result = addTwoNumbers(a, b);
+
+        Assertions.assertEquals(8, result);
+
+        assertThat("Apple")
+                .isEqualTo("Apple")
+                .startsWith("App")
+                .endsWith("e")
+                .hasSize(5);
+    }
+
+    @Test
+//	@DisplayName("displayTestNameTwo")
+    void testDivideTwoNumbers_whenDenominatorIsZero_ThenArithmeticException() {
+
+        int a = 5;
+        int b = 0;
+
+        assertThatThrownBy(() -> divideTwoNumbers(a, b))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("Tried to divide by zero");
+
+    }
+
+    double divideTwoNumbers(int a, int b) {
+        try {
+            return a/b;
+        } catch (ArithmeticException e) {
+            log.error("Arithmentic excepiton occured: "+e.getLocalizedMessage());
+            throw new ArithmeticException("Tried to divide by zero");
+        }
+    }
+
+    int addTwoNumbers(int a, int b) {
+        return a+b;
+    }
 
 }
